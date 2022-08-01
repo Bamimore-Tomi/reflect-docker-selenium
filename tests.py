2c7bb4edea8ef8f26445cff4641ed8edc486f9db
a <tomibami2020@gmail.com>
2022-08-01 00:37:08 -0600

    ad

--- tests.py
+++ tests.py
@@ -12,11 +12,13 @@ chrome_prefs["profile.default_content_settings"] = {"images": 2}
 # Initialize a new chrome driver instance
 driver = webdriver.Chrome(options=chrome_options)
 
-driver.get('https://www.google.com/')
-response_start = driver.execute_script("return window.performance.timing.responseStart")
-dom_complete = driver.execute_script("return window.performance.timing.domComplete")
- 
-load_time = dom_complete - response_start
- 
-print("Load time", load_time)
+driver.get('https://www.saucedemo.com/')
+username = driver.find_element_by_id("user-name")
+password = driver.find_element_by_id("password")
+login_button = driver.find_element_by_id("login-button")
+username.send_keys("standard_user")
+username.send_keys("secret_sauce")
+login_button.click()
+print(driver.current_url())
+assert driver.current_url()=="https://www.saucedemo.com/inventory.html"
 driver.quit()
